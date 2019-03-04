59bcc7aa9ed46704a944d1e2a789431fa2f4b504
Jesse Dijkman <jesse.dijkman@hva.nl>
2019-03-04 14:09:45 +0100

    Added the OBA wrapper by MaanLamp

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,17 @@
+function handleData(responses) {
+  console.log(responses)
+}
 
+(async () => {
+	localStorage.clear();
+
+	const api = new API({
+		key: "1e19898c87464e239192c8bfe422f280"
+	});
+
+	const stream = await api.createStream("search/banaan{5}");
+
+	stream
+		.pipe(handleData)
+		.catch(console.error);
+})();
